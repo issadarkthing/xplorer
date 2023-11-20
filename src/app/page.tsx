38f5cfab9bb6251936fa469c135a11f6facc1cd6
raziman <raziman.terra@gmail.com>
2023-11-21 05:15:46 +0800

    rename file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { FileBrowser } from "@/components/file-browser";
+import { FileBrowser } from "@/components/FileBrowser";
 
 export default async function Home() {
     return <FileBrowser />;
